56fd82de753b89f4cd856d4554405aafcb0325f8
johansen.keith <johansen.keith@gmail.com>
2009-06-26 21:14:00 +0000

    Clean up

--- distMatrix.m
+++ distMatrix.m
@@ -2,13 +2,11 @@ function d=distMatrix(A,B)
 [hA,wA]=size(A);
 [hB,wB]=size(B);
 for k=1:wA
-    %declare 'array' of submatrices
     C{k}= repmat(A(:,k),1,hB);
     D{k}= repmat(B(:,k),1,hA);
 endfor
 S=zeros(hA,hB);
-for k=1:wA
-    %euclidean distance 
+for k=1:wA`
     S=S+(C{k}-D{k}').^2;
 endfor
 d=sqrt(S);
