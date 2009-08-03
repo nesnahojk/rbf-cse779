6cee00bab73edc4d793477c1a56e397da4b3958c
johansen.keith@gmail.com <johansen.keith@gmail.com@a18bb700-56b2-11de-992f-fdd7e25b9163>
2009-08-03 18:08:43 +0000

    Cleanup

--- trunk/RBFPredict.m
+++ trunk/RBFPredict.m
@@ -2,7 +2,7 @@ function y=RBFPredict(nx,means,bandwidths,weights,bias)
 
 sum=0;
 for i=1:length(means)
-    sum=sum+weights(i)*gaussian(means(i),nx,bandwidths(i));
+  sum=sum+weights(i)*gaussian(means(i),nx,bandwidths(i));
 endfor
 
 y=sum+bias
--- trunk/distMatrix.m
+++ trunk/distMatrix.m
@@ -2,11 +2,11 @@ function d=distMatrix(A,B)
 [hA,wA]=size(A);
 [hB,wB]=size(B);
 for k=1:wA
-    C{k}= repmat(A(:,k),1,hB);
-    D{k}= repmat(B(:,k),1,hA);
+  C{k}= repmat(A(:,k),1,hB);
+  D{k}= repmat(B(:,k),1,hA);
 endfor
 S=zeros(hA,hB);
 for k=1:wA`
-    S=S+(C{k}-D{k}').^2;
+  S=S+(C{k}-D{k}').^2;
 endfor
 d=sqrt(S);
